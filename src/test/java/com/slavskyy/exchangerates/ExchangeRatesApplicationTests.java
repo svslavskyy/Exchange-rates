4f06923ee1532daf376811f86b1d26cd16b0f3f8
svslavskyy <47915782+svslavskyy@users.noreply.github.com>
2023-04-28 11:40:26 +0300

    initial commit

---
+++ src/test/java/com/slavskyy/exchangerates/ExchangeRatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slavskyy.exchangerates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
